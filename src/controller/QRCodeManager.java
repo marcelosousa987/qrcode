01c35f990c4878e363be8cb7dd432eb8997a16a4
Marcelo Sousa <marcelosousa987@gmail.com>
2016-10-23 22:32:41 -0200

    Salvar e abrir arquivo de imagem

---
+++ src/controller/QRCodeManager.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import model.QRCodeModel;
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
+import view.MainFrame;
+
+/**
+ *
+ * @author Phoenix
+ */
+public class QRCodeManager {
+
+    public boolean gerarQRCode(QRCodeModel qrc) {
+        try (
+                FileOutputStream f = new FileOutputStream(qrc.getFileName()); // carregar stream
+                ByteArrayOutputStream out = QRCode.from(qrc.getQRCodeText()) // array do formato do QRCode
+                .to(ImageType.PNG)
+                .withSize(qrc.getImageWidth(), qrc.getImageHeight())
+                .stream();) {
+            f.write(out.toByteArray());
+            f.flush();
+            f.close();
+            return true;
+        } catch (IOException ex) {
+            Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public boolean decodificarQRCode(QRCodeModel qrc) {
+        try {
+            BinaryBitmap bitmap = new BinaryBitmap(
+                    new HybridBinarizer(
+                            new BufferedImageLuminanceSource(
+                                    ImageIO.read(new FileInputStream(qrc.getFileName())))));
+            Result decQRCode = new MultiFormatReader().decode(bitmap);
+            qrc.setQRCodeText(decQRCode.getText());
+            return true;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+
+    public boolean exibirQRCode(QRCodeModel qrc) { //função para atribuir o QRCode ao model
+        try {
+            File sourceimage = new File(qrc.getFileName());
+            qrc.setImage(ImageIO.read(sourceimage));
+            return true;
+        } catch (IOException ex) {
+            Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+}
---
+++ src/model/QRCodeModel.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+
+/**
+ *
+ * @author Phoenix
+ */
+public class QRCodeModel {
+    private String fileName;
+    private String qrCodeText;
+    private int imageWidth;
+    private int imageHeight;
+    private BufferedImage image;
+    
+    public int getImageWidth() {
+        return imageWidth;
+    }
+
+    public void setImageSize(int imageWidth, int imageHeight) {
+        this.imageWidth = imageWidth;
+        this.imageHeight = imageHeight;
+    }
+
+    public int getImageHeight() {
+        return imageHeight;
+    }
+
+    public void setImageHeight(int imageHeight) {
+        this.imageHeight = imageHeight;
+    }
+
+
+    public QRCodeModel() {
+        this.image = null;
+    }
+    
+    public void setImage(BufferedImage image){
+        this.image = image;
+    }
+    
+    public Image getImage(){
+        return image;
+    }
+    
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getQRCodeText() {
+        return qrCodeText;
+    }
+
+    public void setQRCodeText(String qrCodeText) {
+        this.qrCodeText = qrCodeText;
+    }
+}
